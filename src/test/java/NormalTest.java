c48a9394db53da9d6d05ba0eb250d6cdc2cf52ad
a0916 <a0916720349@gmail.com>
2025-07-01 22:33:19 +0800

    1

--- src/test/java/NormalTest.java
+++ src/test/java/NormalTest.java
@@ -1,16 +1,29 @@
 import org.example.ssmtest.SsmtestApplication;
+import org.example.ssmtest.mapper.UserMapper;
+import org.example.ssmtest.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest(classes = SsmtestApplication.class)
 public class NormalTest {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private UserMapper userMapper;
     @Test
     public void test() {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        String encode = passwordEncoder.encode("123456");
-        System.out.println(encode);
+        stringRedisTemplate.opsForValue().set("QQ","AA",5, TimeUnit.MINUTES);
+//        User user = userMapper.selectById(1);
+//        System.out.println(user);
+
 
     }
 }
